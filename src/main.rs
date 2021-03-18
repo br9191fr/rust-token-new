61048a116dc9bbd1c39a3c3d6c9c38d8205c7223
Bruno Ricci <brricci@orange.fr>
2021-03-18 20:25:37 +0100

    Add digest computation

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,15 @@ extern crate reqwest;
 extern crate serde_json;
 
 use std::fmt;
+use std::fs::File;
+use std::io::{BufReader, Read, Write};
 use reqwest::{Client,StatusCode};
+use ring::digest::{Context, Digest, SHA256};
 use serde_json::{json, Error};
 use serde::{Deserialize};
 use std::fs::read_to_string;
+
+
 
 #[derive(Deserialize,Debug)]
 struct Token {
@@ -33,12 +38,26 @@ impl std::fmt::Display for EasError {
         writeln!(f, "message: {}", self.Message)
     }
 }
+fn sha256_digest<R: Read>(mut reader: R) -> Result<Digest,Error> {
+    let mut context = Context::new(&SHA256);
+    let mut buffer = [0; 1024];
 
+    loop {
+        if let Ok(count) = reader.read(&mut buffer) {
+            if count == 0 {
+                break;
+            }
+            context.update(&buffer[..count]);
+        }
+    }
+
+    Ok(context.finish())
+}
 /*
 Tests des web services d'EAS
  */
 async fn eas_get_token(display : bool) -> Result<EasResult, reqwest::Error> {
-    let request_url = "https://appdev.cecurity1.com/EAS.INTEGRATOR.API/service/authenticate";
+    let request_url = "https://appdev.cecurity.com/EAS.INTEGRATOR.API/service/authenticate";
     println!("Start get token");
     let payload = json!({
     "appId":"f33c398c-0f77-4351-9f92-1e20fa3fd2f8",
@@ -52,23 +71,23 @@ async fn eas_get_token(display : bool) -> Result<EasResult, reqwest::Error> {
         .json(&payload)
         .send().await?;
     let sc = response.status();
-    if (display) {
+    if display {
         let headers = response.headers();
         for (key, value) in headers.iter() {
             println!("{:?}: {:?}", key, value);
         }
     }
     let body = response.text().await.unwrap();
-    if (!sc.is_success()) {
+    if !sc.is_success() {
         println!("Request failed => {}",sc);
-        let status_string = sc.as_str();
+
         let r: Result<EasError, Error> = serde_json::from_str(&body);
         let r_final = match r {
             Ok(res) => {
                 //println!("EAS error: => {}",res);
                 EasResult::EasError(res)
             },
-            Err(e) => {
+            Err(_e) => {
                 //println!("EAS error???: => {}",e);
                 EasResult::None
             }
@@ -78,7 +97,7 @@ async fn eas_get_token(display : bool) -> Result<EasResult, reqwest::Error> {
 
 
 
-    if (display) {
+    if display {
         // Affiche le statut
         println!("Status : {:#?}", sc);
         // Affiche le body <=> jeton
@@ -90,13 +109,13 @@ async fn eas_get_token(display : bool) -> Result<EasResult, reqwest::Error> {
     let r: Result<Token, Error> = serde_json::from_str(&body);
     let t: EasResult = match r {
         Ok(res) => EasResult::Token(res),
-        Err(e) => EasResult::None
+        Err(_e) => EasResult::None
     };
     println!("stop get token");
     Ok(t)
 }
 
-async fn eas_process() -> Result<(bool), reqwest::Error > {
+async fn eas_process(filename: &str) -> Result<bool, reqwest::Error > {
     let opt_t = eas_get_token(false).await;
 
     let (eas_r,status) = match opt_t {
@@ -118,11 +137,25 @@ async fn eas_process() -> Result<(bool), reqwest::Error > {
         }
     };
 
-    if (!status) {
+    if !status {
         println!("Failed to get token. End eas process !");
         return Ok(false);
     }
-
+    println!("Start digest computation for {}", filename);
+    if let Ok(input_file) = File::open((filename)) {
+        println!("Digest computation step 2");
+        let reader = BufReader::new(input_file);
+        println!("Digest computation step 3");
+        if let Ok(digest) = sha256_digest(reader) {
+            println!("SHA256 Digest is {:#?}",digest);
+        }
+        else {
+            println!("Error while digest computation");
+        }
+    }
+    else {
+        println!("Error openning file {}",filename);
+    }
     return Ok(true);
 }
 
@@ -130,7 +163,7 @@ async fn eas_process() -> Result<(bool), reqwest::Error > {
 async fn main() {
     //send_sms().await;
     //get_data().await;
-    let final_result = eas_process().await;
+    let final_result = eas_process("/Users/bruno/dvlpt/rust/test.txt").await;
     match final_result {
         Ok(true) =>  println!("eas test is ok"),
         Ok(false) => println!("eas test failed"),
